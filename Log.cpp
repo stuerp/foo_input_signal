9bc568a953a1207b9a2c11ea2e6135327b099643
Peter Stuer <peter.stuer@outlook.com>
2025-10-08 14:35:48 +0200

    Fixed memory leak in Log.

--- Log.cpp
+++ Log.cpp
@@ -1,44 +1,45 @@
 
-/** $VER: Log.cpp (2025.09.29) P. Stuer - Another logger implementation **/
+/** $VER: Log.cpp (2025.10.08) P. Stuer - Another logger implementation **/
 
 #include "pch.h"
 
 #include "Log.h"
 
-class NullLog : public ILog
+class null_log_t : public ilog_t
 {
 public:
-    NullLog() noexcept { };
+    null_log_t() noexcept { };
 
-    NullLog(const NullLog &) = delete;
-    NullLog(const NullLog &&) = delete;
-    NullLog & operator=(const NullLog &) = delete;
-    NullLog & operator=(NullLog &&) = delete;
+    null_log_t(const null_log_t &) = delete;
+    null_log_t(const null_log_t &&) = delete;
+    null_log_t & operator=(const null_log_t &) = delete;
+    null_log_t & operator=(null_log_t &&) = delete;
 
-    virtual ~NullLog() final { };
+    virtual ~null_log_t() final { };
 
-    ILog & SetLevel(LogLevel level) noexcept override final { return *this; }
+    ilog_t & SetLevel(LogLevel level) noexcept override final { return *this; }
     LogLevel GetLevel() const noexcept override final { return LogLevel::Never; }
 
-    ILog & AtFatal() noexcept override final { return *this; }
-    ILog & AtError() noexcept override final { return *this; }
-    ILog & AtWarn() noexcept override final { return *this; }
-    ILog & AtInfo() noexcept override final { return *this; }
-    ILog & AtDebug() noexcept override final { return *this; }
-    ILog & AtTrace() noexcept override final { return *this; }
-    ILog & Write(const char * format, ... ) noexcept override final { return *this; }
-    ILog & Write(const char * format, va_list args) noexcept override final { return *this; }
+    ilog_t & AtFatal() noexcept override final { return *this; }
+    ilog_t & AtError() noexcept override final { return *this; }
+    ilog_t & AtWarn() noexcept override final { return *this; }
+    ilog_t & AtInfo() noexcept override final { return *this; }
+    ilog_t & AtDebug() noexcept override final { return *this; }
+    ilog_t & AtTrace() noexcept override final { return *this; }
+    ilog_t & Write(const char * format, ... ) noexcept override final { return *this; }
+    ilog_t & Write(const char * format, va_list args) noexcept override final { return *this; }
 };
 
-ILog & Null = *new NullLog();
+static null_log_t _Null;
+ilog_t & Null = _Null;
 
-class log_impl_t : public ILog
+class log_impl_t : public ilog_t
 {
 public:
 #ifdef _DEBUG
     log_impl_t() noexcept { SetLevel(LogLevel::Debug); }
 #else
-    log_impl_t() noexcept { SetLevel(LogLevel::Info); }
+    LogImpl() noexcept { SetLevel(LogLevel::Info); }
 #endif
 
     log_impl_t(const log_impl_t &) = delete;
@@ -48,7 +49,7 @@ public:
 
     virtual ~log_impl_t() noexcept { };
 
-    ILog & SetLevel(LogLevel level) noexcept override final
+    ilog_t & SetLevel(LogLevel level) noexcept override final
     {
         _Level = level;
 
@@ -60,37 +61,37 @@ public:
         return _Level;
     }
 
-    ILog & AtFatal() noexcept override final
+    ilog_t & AtFatal() noexcept override final
     {
         return (_Level >= LogLevel::Fatal) ? *this : Null;
     }
 
-    ILog & AtError() noexcept override final
+    ilog_t & AtError() noexcept override final
     {
         return (_Level >= LogLevel::Error) ? *this : Null;
     }
 
-    ILog & AtWarn() noexcept override final
+    ilog_t & AtWarn() noexcept override final
     {
         return (_Level >= LogLevel::Warn) ? *this : Null;
     }
 
-    ILog & AtInfo() noexcept override final
+    ilog_t & AtInfo() noexcept override final
     {
         return (_Level >= LogLevel::Info) ? *this : Null;
     }
 
-    ILog & AtDebug() noexcept override final
+    ilog_t & AtDebug() noexcept override final
     {
         return (_Level >= LogLevel::Debug) ? *this : Null;
     }
 
-    ILog & AtTrace() noexcept override final
+    ilog_t & AtTrace() noexcept override final
     {
         return (_Level >= LogLevel::Trace) ? *this : Null;
     }
 
-    ILog & Write(const char * format, ... ) noexcept override final
+    ilog_t & Write(const char * format, ... ) noexcept override final
     {
         char Line[1024] = { };
 
@@ -98,10 +99,13 @@ public:
 
         va_start(args, format);
 
-        return Write(format, args);
+        (void) ::vsnprintf(Line, sizeof(Line) - 1, format, args);
+        console::print(Line);
+
+        return *this;
     }
 
-    ILog & Write(const char * format, va_list args) noexcept override final
+    ilog_t & Write(const char * format, va_list args) noexcept override final
     {
         char Line[1024] = { };
 
@@ -115,6 +119,7 @@ private:
     LogLevel _Level;
 };
 
-ILog & Log = *new log_impl_t();
+static log_impl_t _LogImpl;
+ilog_t & Log = _LogImpl;
 
 cfg_var_modern::cfg_int CfgLogLevel({ 0x1ead47d9, 0xc4aa, 0x4079, { 0x95, 0xfa, 0xaf, 0x51, 0x6a, 0xaf, 0xd8, 0xb6 } }, LogLevel::Debug);
